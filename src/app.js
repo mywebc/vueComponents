2a6a61f3e78d44af4c692253990f86ab026f46bc
陈晓拉尼 <2524729719@qq.com>
2018-11-27 00:52:06 +0800

    新建一个图标组件

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
 
 Vue.component('g-button', Button);
+Vue.component('g-icon', Icon);
+
 
 new Vue({
     el: '#app'
---
+++ src/icon.vue
@@ -0,0 +1,18 @@
+<template>
+    <svg class="g-icon">
+        <use :xlink:href=`#i-${name}`></use>
+    </svg>
+</template>
+
+<script>
+    export default {
+        props: ['name']
+    }
+</script>
+
+<style lang="scss">
+    .g-icon {
+        width: 1em;
+        height: 1em;
+    }
+</style>
