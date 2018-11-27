aa9fdaee6bf694d66b978b63a7ca2ff2ef7606f7
陈晓拉尼 <2524729719@qq.com>
2018-11-28 00:07:34 +0800

    为按钮添加click事件

--- src/app.js
+++ src/app.js
@@ -7,5 +7,10 @@ Vue.component('g-icon', Icon);
 
 
 new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        loading1: false,
+        loading2: false,
+        loading3: false,
+    }
 })
--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-        <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
-        <g-icon name="loading" class="loading"></g-icon>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+        <g-icon v-if="icon && !loading" :name="icon" class="icon"></g-icon>
+        <g-icon name="loading" class="loading icon" v-if="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -13,6 +13,10 @@
         // props: ["icon","iconPosition"]
         props: {
             icon: {},
+            loading: {
+                type: Boolean,
+                default: false
+            },
             iconPosition: {
                 type: String,
                 default: 'left',
