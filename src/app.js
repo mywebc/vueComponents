700533349963654daf4b990ab92844be5257e0de
陈晓拉尼 <2524729719@qq.com>
2018-11-28 23:44:30 +0800

    使用chai.expect添加四个测试用例

--- src/app.js
+++ src/app.js
@@ -16,3 +16,85 @@ new Vue({
         loading3: false,
     }
 })
+
+// 单元测试
+import chai from 'chai'
+const expect = chai.expect
+// 测试icon
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: "settings"
+        }
+    }).$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq("#i-settings")
+    vm.$el.remove()
+    vm.$destroy()
+}
+// 测试loading
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: "settings",
+            loading: true
+        }
+    }).$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq("#i-loading")
+    vm.$el.remove()
+    vm.$destroy()
+}
+// 测试order
+// order为1的情况
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: "settings"
+        }
+    }).$mount(div)
+    let svgElement = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svgElement)
+    expect(order).to.eq("1")
+    vm.$el.remove()
+    vm.$destroy()
+}
+// order为2的情况
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: "settings",
+            iconPosition: "right"
+        }
+    }).$mount(div)
+    let svgElement = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svgElement)
+    expect(order).to.eq("2")
+    vm.$el.remove()
+    vm.$destroy()
+}
+// 测试点击事件
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: "settings",
+            iconPosition: "right"
+        }
+    }).$mount()
+    vm.$on('click', function () {
+        expect(1).to.eq(1)
+    })
+    let button = vm.$el
+    button.click()
+}
--- src/button-group.vue
+++ src/button-group.vue
@@ -9,7 +9,7 @@
         mounted() {
             for (let node of this.$el.children) {
                 let name = node.nodeName.toLowerCase()
-                if (name !== ' button') {
+                if (name !== "button") {
                     console.warn('g-button-group的子元素应该是button');
                 }
             }
@@ -23,7 +23,9 @@
         vertical-align: middle;
         > .g-button {
             border-radius: 0;
-            margin-left: -1px;
+            &:not(:first-child) {
+                margin-left: -1px;
+            }
             &:first-child {
                 border-top-left-radius: var(--border-radius);
                 border-bottom-left-radius: var(--border-radius);
