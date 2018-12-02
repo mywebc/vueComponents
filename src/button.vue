5849835b7e39bf1831d33d1f5407c031e9e575f1
陈晓拉尼 <2524729719@qq.com>
2018-12-02 13:31:28 +0800

    使用karma自动化测试

--- src/button.vue
+++ src/button.vue
@@ -9,8 +9,10 @@
 </template>
 
 <script>
+    import Icon from './icon'
     export default {
         // props: ["icon","iconPosition"]
+        components: {"g-icon": Icon},
         props: {
             icon: {},
             loading: {
