742b1b532f989848bedda924ad01ab0ea852b90c
陈晓拉尼 <2524729719@qq.com>
2019-01-28 19:46:01 +0800

    row和col大体搞定

---
+++ src/row.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "row"
+    }
+</script>
+
+<style scoped lang="scss">
+    .row {
+        display: flex;
+    }
+</style>
