815a7077f0d09950ab769fae463a38542801c7fa
陈晓拉尼 <2524729719@qq.com>
2019-01-31 21:15:29 +0800

    使用单例模式保证每次只创建一个toast

--- src/plugin.js
+++ src/plugin.js
@@ -1,17 +1,27 @@
 import Toast from './toast'
 
+
+// 提取函数
+function createToast({Vue, message, propsData}) {
+    // 生成一个toast组件放到body里面
+    let Construstor = Vue.extend(Toast)
+    let toast = new Construstor({propsData})
+    // 把信息给到插槽
+    toast.$slots.default = [message]
+    toast.$mount()
+    document.body.appendChild(toast.$el)
+    return toast
+}
+// 设置标记变量
+let currentToast
 export default {
     install (Vue, options) {
         Vue.prototype.$toast = function (message, toastOptions) {
-            // 生成一个toast组件放到body里面
-            let Construstor = Vue.extend(Toast)
-            let toast = new Construstor({
-                propsData: toastOptions
-            })
-            // 把信息给到插槽
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            if (currentToast) {
+                console.log(currentToast)
+                currentToast.close()
+            }
+            currentToast = createToast({Vue, message, propsData: toastOptions})
         }
     }
 }
