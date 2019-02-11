377bbcfdaf3c751d875c68bc0728fdecc8773d0e
陈晓拉尼 <2524729719@qq.com>
2019-02-11 18:22:55 +0800

    添加部分测试用例

---
+++ test/tabs-head.test.js
@@ -0,0 +1,22 @@
+const expect = chai.expect;
+import Vue from 'vue'
+import Tabs from '../src/tabs'
+import TabsHead from '../src/tabs-head'
+import TabsBody from '../src/tabs-body'
+import TabsItem from '../src/tabs-item'
+import TabsPane from '../src/tabs-pane'
+
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-head', TabsHead)
+Vue.component('g-tabs-body', TabsBody)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-tabs-pane', TabsPane)
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('TabsHead', () => {
+    it('存在.', () => {
+        expect(TabsHead).to.exist
+    })
+})
