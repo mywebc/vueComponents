d55f83017b1e1dd177cea7c373b001e200184ff9
陈晓拉尼 <2524729719@qq.com>
2019-02-11 19:47:01 +0800

    阻止click冒泡

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="xxx">
+        <div class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content" ></slot>
         </div>
         <slot></slot>
