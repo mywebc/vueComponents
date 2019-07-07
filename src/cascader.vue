da9bd96d6cf99613a90b0bf1221803e6a01b053d
陈晓拉尼 <2524729719@qq.com>
2019-07-07 19:26:36 +0800

    下拉列表增加可设置属性height,初步调整样式

--- src/cascader.vue
+++ src/cascader.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="cascader">
     <div class="trigger" @click="poppoverVisible = !poppoverVisible"></div>
-    <div class="popover" v-if="poppoverVisible">
-        <cascader-items :items="source"></cascader-items>
+    <div class="popover" v-if="poppoverVisible" :style="{height: `${height}px`}">
+      <cascader-items :items="source" :height="height"></cascader-items>
     </div>
   </div>
 </template>
@@ -15,33 +15,35 @@ export default {
   props: {
     source: {
       type: Array
+    },
+    height: {
+      type: Number
     }
   },
   data() {
     return {
-      poppoverVisible: false,
+      poppoverVisible: false
     };
-  },
+  }
 };
 </script>
 
 <style scoped lang="scss">
 @import "var.scss";
 .cascader {
+  position: relative;
   .trigger {
     width: 80px;
     height: 32px;
-    border: 1px solid red;
+    border: 1px solid #000;
   }
   .popover {
+    background: #fff;
+    position: absolute;
+    top: 100%;
+    left: 0;
     width: 200px;
-    height: 100px;
-    border: 1px solid red;
     display: flex;
-    .label {
-      width: 60px;
-      border: 1px solid #000;
-    }
   }
 }
 </style>
--- src/cascaderI-items.vue
+++ src/cascaderI-items.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="cascaderItems">
     <div class="left">
-      <div v-for="item in items" @click="leftSelected = item">
+      <div v-for="item in items" @click="leftSelected = item" class="label">
         {{ item.name }}
+        <icon class="icon" v-if="item.children" name="right" />
       </div>
     </div>
     <div class="right" v-if="rightItems">
@@ -12,11 +13,16 @@
 </template>
 
 <script>
+import Icon from "./icon";
 export default {
   name: "cascaderItems",
+  components: { Icon },
   props: {
     items: {
       type: Array
+    },
+    height: {
+      type: Number
     }
   },
   data() {
@@ -35,15 +41,33 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "var";
 .cascaderItems {
-  border: 1px solid red;
+  height: 100%;
+  @extend .box-shadow;
   margin: 10px;
   display: flex;
   justify-content: flex-start;
   align-items: flex-start;
   margin: 0;
+  .left {
+    height: 100%;
+    padding: 0.3em 0;
+    .label {
+      white-space: nowrap;
+      padding: 0.3em 1em;
+      display: flex;
+      align-items: center;
+      .icon {
+        margin-left: 1em;
+        transform: scale(.5);
+      }
+    }
+  }
   .right {
+    height: 100%;
     margin-top: -1px;
+    border-left: 1px solid $border-color-light;
   }
 }
 </style>
