dca37e7a9c961d661038401de97327919c12314f
陈晓拉尼 <2524729719@qq.com>
2019-07-08 15:59:38 +0800

    因为scss编译过慢，将构建工具parcel改为vue-cli3

--- src/demo.vue
+++ src/demo.vue
@@ -1,13 +1,68 @@
 <template>
-    <div>测试组件1234</div>
+  <div>
+    <g-cascader :source="source" :height="200"></g-cascader>
+    <p>123</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "demo"
-    }
+import Cascader from "./cascader";
+export default {
+  components: {
+    "g-cascader": Cascader
+  },
+  name: "demo",
+  data() {
+    return {
+      source: [
+        {
+          name: "江苏省",
+          children: [
+            {
+              name: "南通市",
+              children: [{ name: "开发区" }, { name: "崇川区" }]
+            },
+            {
+              name: "宿迁市",
+              children: [{ name: "宿迁区" }]
+            }
+          ]
+        },
+        {
+          name: "浙江省",
+          children: [
+            {
+              name: "湖州市",
+              children: [{ name: "湖州区" }, { name: "湖州2区" }]
+            },
+            {
+              name: "杭州市",
+              children: [{ name: "杭州一区" }, { name: "杭州二区" }]
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+/*原生定义CSS变量*/
+:root {
+  --button-height: 32px;
+  --font-size: 14px;
+  --button-bg: white;
+  --button-active-bg: #eee;
+  --border-radius: 4px;
+  --color: #333;
+  --border-color: #999;
+  --border-color-hover: #666;
+}
 </style>
