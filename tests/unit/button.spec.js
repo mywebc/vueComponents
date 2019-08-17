fce84e325dea54cf32a3a814daca17db7433a9bd
陈晓拉尼 <2524729719@qq.com>
2019-08-17 09:20:04 +0800

    改造button和popover两个单元测试

---
+++ tests/unit/button.spec.js
@@ -0,0 +1,71 @@
+import chai, {expect} from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import {shallowMount, mount} from '@vue/test-utils'
+import Button from '@/button.vue'
+chai.use(sinonChai)
+
+describe('Button.vue', () => {
+    it('存在.', () => {
+        expect(Button).to.exist
+    })
+    it('可以设置icon.', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'settings'
+            }
+        })
+        const useElement = wrapper.find('use')
+        expect(useElement.attributes()['href']).to.equal('#i-settings')
+    })
+    it('可以设置loading.', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'settings',
+                loading: true
+            }
+        })
+        const vm = wrapper.vm
+        const useElements = vm.$el.querySelectorAll('use')
+        expect(useElements.length).to.equal(1)
+        expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
+    })
+
+    xit('icon 默认的 order 是 1', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'settings',
+            }
+        })
+        const vm = wrapper.vm
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('1')
+    })
+
+    xit('设置 iconPosition 可以改变 order', () => {
+        const wrapper = mount(Butotn, {
+            propsData: {
+                icon: 'settings',
+                iconPosition: 'right'
+            }
+        })
+        const vm = wrapper.vm
+        const icon = vm.$el.querySelector('svg')
+        expect(getComputedStyle(icon).order).to.eq('2')
+    })
+    it('点击 button 触发 click 事件', () => {
+        const wrapper = mount(Button, {
+            propsData: {
+                icon: 'settings',
+            }
+        })
+        const vm = wrapper.vm
+
+
+        const callback = sinon.fake();
+        vm.$on('click', callback)
+        vm.$el.click()
+        expect(callback).to.have.been.called
+
+    })
+})
